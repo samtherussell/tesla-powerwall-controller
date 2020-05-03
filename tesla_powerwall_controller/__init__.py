9e24fdfad3db5e07e6c1f7ddfabfe4b69759919f
Samuel Russell <sr14978@bristol.ac.uk>
2020-05-03 16:24:36 +0100

    added power now measurements

--- tesla_powerwall_controller/__init__.py
+++ tesla_powerwall_controller/__init__.py
@@ -4,21 +4,77 @@ import argparse
 
 protocol = "https://"
 base_api_path = "/api"
-power_level_path = base_api_path + "/system_status/soe"
+battery_level_path = base_api_path + "/system_status/soe"
+power_levels_path = base_api_path + "/meters/aggregates"
+
 
 class PowerwallController:
   def __init__(self, host):
     self.base_url = protocol + host
 
-  def get_battery_level(self):
-    req = requests.get(self.base_url + power_level_path, verify=False)
+  def get_battery_charge(self):
+    req = requests.get(self.base_url + battery_level_path, verify=False)
     content = req.content.decode("utf-8")
     result = json.loads(content)
-    return "The battery is at {} percent".format(int(result['percentage']))
-  
+    val = int(result['percentage'])
+    return f"The battery is at {val} percent"
+
+  def get_battery_power(self):
+    req = requests.get(self.base_url + power_levels_path, verify=False)
+    content = req.content.decode("utf-8")
+    result = json.loads(content)
+    val = int(result['battery']['instant_power'])
+    if val == 0:
+      return "The battery is not being used"
+    if val < 0:
+      return f"The battery is charging at {-val} watts"
+    else:
+      return f"The battery is supplying {val} watts"
+
+  def get_solar_power(self):
+    req = requests.get(self.base_url + power_levels_path, verify=False)
+    content = req.content.decode("utf-8")
+    result = json.loads(content)
+    val = int(result['solar']['instant_power'])
+    return f"The solar panels are supplying {val} watts"
+
+  def get_grid_power(self):
+    req = requests.get(self.base_url + power_levels_path, verify=False)
+    content = req.content.decode("utf-8")
+    result = json.loads(content)
+    val = int(result['site']['instant_power'])
+    if val == 0:
+      return "The grid is not being used"
+    if val < 0:
+      return f"You are exporting {-val} watts"
+    else:
+      return f"You are importing {val} watts"
+
+  def get_house_power(self):
+    req = requests.get(self.base_url + power_levels_path, verify=False)
+    content = req.content.decode("utf-8")
+    result = json.loads(content)
+    val = int(result['load']['instant_power'])
+    return f"The house is using {val} watts"
+
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument('host')
+  parser.add_argument('measurement', choices=['charge', 'battery', 'solar', 'grid', 'house'])
   args = parser.parse_args()
-  con = TygarwenPowerwallController(args.host)
-  print(con.get_battery_level())
+  con = PowerwallController(args.host)
+  if args.measurement == 'charge':
+    method = con.get_battery_charge
+  elif args.measurement == 'battery':
+    method = con.get_battery_power
+  elif args.measurement == 'solar':
+    method = con.get_solar_power
+  elif args.measurement == 'grid':
+    method = con.get_grid_power
+  elif args.measurement == 'house':
+    method = con.get_house_power
+  else:
+    raise Exception("invalid option")
+
+  print(method())
+    
